67fb43323743827dd6b1e62dc7e080c0b61b0f79
Pratima Singh <pxs@amazon.com>
2024-01-22 02:51:00 +0000

    V2

--- windows-sysmon/windows_sysmon_custom_source.sh
+++
@@ -1,5 +0,0 @@
-aws securitylake create-custom-log-source  \
-   --source-name windows-sysmon \
-   --configuration crawlerConfiguration={"roleArn=<GLUE_IAM_ROLE_ARN>"},providerIdentity={"externalId=CustomSourceExternalId123,principal=<AWS_IDENTITY_PRINCIPAL>"} \
-   --event-classes FILE_ACTIVITY PROCESS_ACTIVITY \
-   --region <SECURITY_LAKE_REGION>
